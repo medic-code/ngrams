266f72c758e2b14bde2331f54b76e4c5a0848c80
medic-code <u40as7@gmail.com>
2024-10-27 14:44:44 +0000

    refactor cli code

--- src/ngram.py
+++ src/ngram.py
@@ -1,7 +1,20 @@
 import argparse 
 
-parser = argparse.ArgumentParser(description='This is a n-grams model command line')
-parser.add_argument('filename', type=str, help='Filename of training corpus')
-parser.add_argument('--s', action='store_true', help='Provides a 50 word sample of the text supplied')
+def main():
+    parser = argparse.ArgumentParser(description='This is a n-grams model command line')
+    parser.add_argument('filename', type=str, help='Filename of training corpus')
+    parser.add_argument('--s', action='store_true', help='Provides a 50 word sample of the text supplied')
 
-args = parser.parse_args()
+    args = parser.parse_args()
+
+    with open(args.filename, 'r') as file:
+        content = file.read()
+        
+
+    if (args.s):
+        print(content[:49])
+
+
+if __name__ == "__main__":
+    main()
+
