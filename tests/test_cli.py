ff9bd90201b3e57ecb7859a4aca8cd8d6f738359
medic-code <u40as7@gmail.com>
2024-10-27 14:57:44 +0000

    Add sample test for 50 words

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,11 +2,20 @@ import subprocess
 import sys
 import os
 
-def test_sample_cli():
+def test_sample_option():
     result = subprocess.run(
         [sys.executable, os.path.join('src','ngram.py'),os.path.join('tests','test.txt'),'--s'], 
         capture_output=True, text=True
     )
-
     assert result.returncode == 0
     assert result.stdout.strip() == "this is just a test"
+
+def test_sample_option_displays_50_words():
+    result = subprocess.run(
+        [sys.executable,os.path.join('src','ngram.py'),os.path.join('tests','test.txt'),'--s'],
+        capture_output=True,
+        text=True
+    )
+
+    assert result.returncode == 0
+    assert len(result.stdout.strip().join(" ")) < 50
